284b4996abaa2fce31c316a623ea0732946714de
yongjian.lu <yongjian.lu@advancegroup.com>
2024-02-23 15:45:20 +0800

    初始版本

---
+++ astro.config.mjs
@@ -0,0 +1,14 @@
+import { defineConfig } from 'astro/config';
+
+import tailwind from "@astrojs/tailwind";
+
+// https://astro.build/config
+export default defineConfig({
+  integrations: [tailwind()],
+  site: 'https://www.luyongjian.site',
+  compressHTML: true,
+  outDir: './dist',
+  build: {
+    format: 'file'
+  }
+});
---
+++ src/scripts/tongji.js
@@ -0,0 +1,17 @@
+// 百度统计
+var _hmt = _hmt || [];
+(function () {
+  var hm = document.createElement("script");
+  hm.src = "https://hm.baidu.com/hm.js?2f5707a5eeec55489e909cf7fb0ecb38";
+  var s = document.getElementsByTagName("script")[0];
+  s.parentNode.insertBefore(hm, s);
+})();
+
+// google统计
+window.dataLayer = window.dataLayer || [];
+function gtag() {
+  dataLayer.push(arguments);
+}
+gtag("js", new Date());
+
+gtag("config", "G-GTXJSR4LH5");
